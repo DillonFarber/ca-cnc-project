ed0cb5eab1aa9a15bfc777c87bb827f9799b27b6
Dillon_Farber <55909397+DillonFarber@users.noreply.github.com>
2022-04-14 23:11:03 -0500

    Merge branch 'main' of https://github.com/DillonFarber/ca-cnc-project

--- ca_project_code/main.cpp
+++ ca_project_code/main.cpp
@@ -6,9 +6,7 @@
 
     #include <A4988.h>
     #include <BasicStepperDriver.h>
-    #include <DRV8825.h>
-    #include <DRV8834.h>
-    #include <DRV8880.h>
+    #include <Servo.h>
     #include <MultiDriver.h>
     #include <SyncDriver.h>
     #include <ezButton.h>
